93c0efed2f1b6865cdc3c71708f19ac51005a48e
Shirokov <sshiroko@t-systems.es>
2023-05-23 12:02:00 +0200

    Installed Typescript for the app module

--- frontend-module/vue.config.js
+++ frontend-module/vue.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const config = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    // entry: './src/main.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
       libraryTarget: 'system',
     },
   },
 })
+
+console.log(JSON.stringify(config, null, 2))
+module.exports = config
